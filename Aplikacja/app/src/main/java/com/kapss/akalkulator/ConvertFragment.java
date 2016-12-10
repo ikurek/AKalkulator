e2a1d75286575fcde54f352e2f38c93200284768
Igor Kurek <igorkurek96@gmail.com>
2016-12-11 00:27:04 +0100

    Simple converter UI

--- Aplikacja/app/src/main/java/com/kapss/akalkulator/ConvertFragment.java
+++ Aplikacja/app/src/main/java/com/kapss/akalkulator/ConvertFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.roughike.swipeselector.SwipeItem;
+import com.roughike.swipeselector.SwipeSelector;
 
 
 public class ConvertFragment extends Fragment {
@@ -29,12 +31,26 @@ public class ConvertFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_convert, container, false);
+
+        //Konfiguracja selectora dla systemu wejściowego
+        //FIXME: To można na pewno napisac ładniej. Napisz to ładniej.
+        SwipeSelector swipeSelectorForInput = (SwipeSelector) view.findViewById(R.id.systemSelectorForInput);
+        SwipeSelector swipeSelectorForOutput = (SwipeSelector) view.findViewById(R.id.systemSelectorForOutput);
+        swipeSelectorForInput.setItems(
+                new SwipeItem(0, "System naturalny", "System liczbowy, w którym podstawa jest liczbą całkowitą"),
+                new SwipeItem(1, "System uzupełnieniowy", "System liczbowy posiadający nieskonczone rozszerzeie lewostronne"));
+
+        swipeSelectorForOutput.setItems(
+                new SwipeItem(0, "System naturalny", "System liczbowy, w którym podstawa jest liczbą całkowitą"),
+                new SwipeItem(1, "System uzupełnieniowy", "System liczbowy posiadający nieskonczone rozszerzeie lewostronne"));
+
         return view;
     }
 
