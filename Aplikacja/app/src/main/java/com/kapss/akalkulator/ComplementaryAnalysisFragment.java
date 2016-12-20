07c06f3c68d1684298931fd0a1e86af9dd684439
Igor Kurek <igorkurek96@gmail.com>
2016-12-20 16:58:46 +0100

    Algorytm sprawdzający znak liczby w uzupełniniowych

--- Aplikacja/app/src/main/java/com/kapss/akalkulator/ComplementaryAnalysisFragment.java
+++ Aplikacja/app/src/main/java/com/kapss/akalkulator/ComplementaryAnalysisFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
@@ -16,8 +18,11 @@ public class ComplementaryAnalysisFragment extends Fragment {
 
     private MathConvertLibrary mathConvertLibrary;
     private MathSupportLibrary mathSupportLibrary;
+    private MathComplementaryLibrary mathComplementaryLibrary;
     private MaterialBetterSpinner systemSelectorForInput;
-    public MaterialEditText inputNumber;
+    public MaterialEditText inputNumberEditText;
+    public TextView textViewSign;
+    public Button analyzeButton;
 
     public ComplementaryAnalysisFragment() {
         // Required empty public constructor
@@ -30,6 +35,7 @@ public class ComplementaryAnalysisFragment extends Fragment {
         //Załaduj MathConvertLibrary i MathSupportLibrary
         mathConvertLibrary = new MathConvertLibrary();
         mathSupportLibrary = new MathSupportLibrary();
+        mathComplementaryLibrary = new MathComplementaryLibrary();
 
 
     }
@@ -40,8 +46,19 @@ public class ComplementaryAnalysisFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_complementary_analysis, container, false);
-
         fillSystemList(view);
+        assignUIElements(view);
+
+
+        analyzeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputNumber = inputNumberEditText.getText().toString();
+                String complementaryBase = systemSelectorForInput.getText().toString();
+
+                textViewSign.setText(getSign(inputNumber, complementaryBase));
+            }
+        });
 
 
         return view;
@@ -54,6 +71,23 @@ public class ComplementaryAnalysisFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         systemSelectorForInput = (MaterialBetterSpinner) view.findViewById(R.id.systemSelectorForInput);
         systemSelectorForInput.setAdapter(adapter);
+
+    }
+
+    private void assignUIElements(View view) {
+
+        analyzeButton = (Button) view.findViewById(R.id.analyzeButton);
+        inputNumberEditText = (MaterialEditText) view.findViewById(R.id.inputNumber);
+        systemSelectorForInput = (MaterialBetterSpinner) view.findViewById(R.id.systemSelectorForInput);
+        textViewSign = (TextView) view.findViewById(R.id.textViewForSign);
+
+
+    }
+
+    private String getSign(String inputNumber, String complementaryBase) {
+
+        if(mathComplementaryLibrary.isComplementarySigned(inputNumber, complementaryBase)) return "-";
+        else return "+";
 
     }
 
--- Aplikacja/app/src/main/java/com/kapss/akalkulator/MathComplementaryLibrary.java
+++ Aplikacja/app/src/main/java/com/kapss/akalkulator/MathComplementaryLibrary.java
@@ -5,4 +5,23 @@ package com.kapss.akalkulator;
  */
 
 public class MathComplementaryLibrary {
+
+    //Konstruktorek jak zwykle
+    public MathComplementaryLibrary () {
+
+    }
+
+    //Funkcja określa, czy liczba w systemie uzupełnieniowym jest ujemna
+    //Przyjmuje liczbę i podstawę w formacie "U" + baza
+    //Zwraca true dla liczb ujemnych
+    //Zwraca false dla liczb dodatnich
+    public Boolean isComplementarySigned(String inputNumber, String complementaryBase){
+
+        //Zbiera wartość bazy i zapisuje jako int
+        Integer baseValue = Integer.valueOf(complementaryBase.substring(1));
+        Integer firstDigit = Integer.valueOf(inputNumber.charAt(0));
+
+        return (baseValue/2 - 1 >= firstDigit);
+
+    }
 }
